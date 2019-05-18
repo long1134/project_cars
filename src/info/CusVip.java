070ed3d35ccce2327f7cf44e25495f0d57297d6a
why-s3rious <17521194@gm.uit.edu.vn>
2019-05-18 16:31:02 +0700

    create renting car

---
+++ src/info/CusVip.java
@@ -0,0 +1,11 @@
+package info;
+
+public class CusVip extends Customer {
+
+    public CusVip(String id,String name,String age,String type,String phone){
+        super(id,name,age,type,phone);
+    }
+
+
+
+}
